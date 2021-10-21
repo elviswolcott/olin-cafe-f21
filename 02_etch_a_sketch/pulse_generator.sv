29a78031f26d7535346f914f9c12a31dcc5f4719
Elvis <ewolcott@olin.edu>
2021-10-21 17:30:26 -0400

    feat: pulse generator

--- 02_etch_a_sketch/pulse_generator.sv
+++ 02_etch_a_sketch/pulse_generator.sv
@@ -1,15 +1,29 @@
 /*
   Outputs a pulse generator with a period of "ticks".
-  out should go high for one cycle ever "ticks" clocks.
+  out should go high for one cycle every "ticks" clocks.
 */
 module pulse_generator(clk, rst, ena, ticks, out);
 
 parameter N = 8;
+
 input wire clk, rst, ena;
 input wire [N-1:0] ticks;
 output logic out;
 
 logic [N-1:0] counter;
 logic counter_comparator;
+
+always_ff @(posedge clk) begin
+  if (rst |  counter_comparator) begin
+    counter <= 1'b0;
+  end else if (ena) begin
+    counter <= counter + 1'b1;
+  end
+end
+
+always_comb begin
+  counter_comparator = counter == ticks - 1;
+  out = counter_comparator & ena & clk;
+end
 
 endmodule
